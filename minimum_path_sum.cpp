46e254c0e6c20f5922b34eba523b3bda19845caf
CodeBag-Debashish <dekadebashish3@gmail.com>
2018-07-29 11:05:41 +0530

    https://leetcode.com/problems/minimum-path-sum/description/

---
+++ minimum_path_sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = (int)(grid.size());
+        int n = (int)(grid[0].size());
+        vector<int> dp(m + 2,0);
+        for(int col = 1;col<=n;col++) {
+            for(int row = 1;row<=m;row++) {
+                if(col == 1) {
+                    dp[row] = dp[row - 1] + grid[row - 1][col - 1];
+                }else {
+                    dp[row] = min( dp[row] , dp[row - 1] ) + grid[row - 1][col - 1];
+                }
+            }
+        }
+        return dp[m];    
+    }
+};
